2b4ae3c5038adc5a27fa7dee2150aa40fc6d9a93
Emmy984 <fsremily01@gmail.com>
2026-05-12 13:33:35 -0300

    aula-06

---
+++ exec10.js
@@ -0,0 +1,6 @@
+let codigo = 6
+while (codigo != 1) {
+ codigo-= 1
+    console.log(`contagem regressiva: ${codigo}`)
+}
+console.log("Contagem regressiva encerrada")
---
+++ exec9.js
@@ -0,0 +1,2 @@
+let linguagens = ["Javascript", "Python", "Flutter"]
+console.log("Minhas linguagens favoritas são:" + linguagens[0], linguagens[1], linguagens[2])
